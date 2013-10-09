1094d3208c4f8b1b152af1d01d13caa6091c7b9b
vgulaev <vgulaev@yandex.ru>
2013-10-09 11:20:20 +0600

    for testing WS

---
+++ cgi/testparams.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python
+import cgi
+import cgitb; cgitb.enable()
+
+print ("Content-Type: text/html; charset=utf-8")
+print ("")
+print ("Hello word")
+
+form = cgi.FieldStorage()
+print(form)
+if form.has_key("dd"):
+	print ("Yes I have dd = ", form["dd"].value)
+#for el in form:
+#    print(el)
