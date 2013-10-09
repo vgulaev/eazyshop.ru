6f5d612a80187d3ec72048a280dfd75b5f86f9d6
vgulaev <vgulaev@yandex.ru>
2013-10-09 11:40:32 +0600

    Up

--- cgi/wses.py
+++ cgi/wses.py
@@ -1,18 +1,21 @@
 #!/usr/local/bin/python
-#
-#This is webservise for eazyshop. It doing somethink
-#
 import cgi
 import cgitb; cgitb.enable()
+import os
+
+#======================================================
+#This is webservise for eazyshop. It doing somethink
+#
 
 print ("Content-Type: text/html; charset=utf-8")
 print ("")
 
 form = cgi.FieldStorage()
-#print(form)
 # o - mean "operation", cut the value name for smaller http request
 if form.has_key("o"):
-	if form["0"].value
-	print ("Yes I have dd = ", form["dd"].value)
-#for el in form:
-#    print(el)
+	# cn - mean "check name"
+	if (form["o"].value == "cn"):
+		if (os.path.isdir("/home/eazyshop/eazyshop.ru/doc/" + form["n"].value)):
+			print("exist")
+		else:
+			print ("free")
