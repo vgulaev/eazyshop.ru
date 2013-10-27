cfbab6b7e4698b6ccd6f5a49d45b86bd6ecf4f43
Valentin <vgulaev@yandex.ru>
2013-10-28 01:50:28 +0600

    Up

---
+++ cgi/getprice.py
@@ -0,0 +1,34 @@
+﻿#!c:/Python27/python.exe
+# -*- coding: utf-8 -*-
+# This Python file uses the following encoding: utf-8
+
+from secrets import *
+from htmlgenerator import * 
+import MySQLdb
+from tarfile import version
+print ("Content-Type: text/html; charset=utf-8")
+print ("")
+
+db = MySQLdb.connect(host="localhost", user=mysqlusername, passwd=mysqlupsw, db="eazyshop_db", charset='utf8')
+
+cursor = db.cursor()
+
+sql = """
+SELECT goods.caption, pricetypes.caption, prices.price FROM prices
+    LEFT JOIN (goods, pricetypes) ON (prices.good = goods.id AND prices.pricetype = pricetypes.id)
+"""
+cursor.execute(sql)
+
+rd = u""
+row = cursor.fetchone()
+#printtablehead(len(row))
+while not row is None:
+    #print type(str(row[0]))
+    rd = rd + wh(u"".join(wh(unicode(e), "tr") for e in row), "td")
+    row = cursor.fetchone()
+
+db.close()
+
+rd = wh(wh(rd, "tbody"), "table")
+
+print rd.encode("utf8")
