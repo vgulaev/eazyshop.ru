4898a7f214243b4e16ca1739a3837fd8511ba13e
Valentin <vgulaev@yandex.ru>
2013-10-27 18:56:18 +0600

    Up

--- cgi/initdb.py
+++ cgi/initdb.py
@@ -16,7 +16,7 @@ def droptable(tablename):
     db.commit()
 
 def dropalltable():
-    names = ["prices", "pricetypes", "goods"]
+    names = ["prices", "pricetypes", "goods", "shops"]
     for e in names:
         if checktableexist(e):
             droptable(e)
@@ -29,47 +29,60 @@ def checktableexist(tablename):
     cursor.execute(sql, tablename)
     return not cursor.fetchone() is None
     
+def createshopstable():
+    sql = """
+    CREATE TABLE shops (
+    id CHAR(40) PRIMARY KEY,
+    caption CHAR(100)
+    ) ENGINE=INNODB CHARACTER SET utf8 COLLATE utf8_bin;
+    """
+    cursor.execute(sql)
+    db.commit()
+
 def creategoodstable():
-    if checktableexist("goods"):
-        droptable("goods")
     sql = """
     CREATE TABLE goods (
     id CHAR(40) PRIMARY KEY,
-    caption CHAR(100)
-    ) ENGINE=INNODB;
+    shop CHAR(40),
+    caption CHAR(100),
+    
+    FOREIGN KEY (shop) REFERENCES shops(id)
+    ) ENGINE=INNODB CHARACTER SET utf8 COLLATE utf8_bin;
     """
     cursor.execute(sql)
     db.commit()
 
 def createpricetypestable():
-    if checktableexist("pricetypes"):
-        droptable("pricetypes")
     sql = """
-    CREATE TABLE goods (
+    CREATE TABLE pricetypes (
     id CHAR(40) PRIMARY KEY,
-    caption CHAR(100)
-    ) ENGINE=INNODB;
+    shop CHAR(40),
+    caption CHAR(100),
+    
+    FOREIGN KEY (shop) REFERENCES shops(id)
+    ) ENGINE=INNODB CHARACTER SET utf8 COLLATE utf8_bin;
     """
     cursor.execute(sql)
     db.commit()
 
 def createpricestable():
-    if checktableexist("prices"):
-        droptable("prices")
     sql = """
     CREATE TABLE prices (
     id INTEGER PRIMARY KEY,
+    pricetype CHAR(40),
     good CHAR(40),
     price DECIMAL(8,2),
     
-    FOREIGN KEY (good)
-        REFERENCES goods(id)    
-    ) ENGINE=INNODB;
+    FOREIGN KEY (good) REFERENCES goods(id),
+    FOREIGN KEY (pricetype) REFERENCES pricetypes(id)
+    ) ENGINE=INNODB CHARACTER SET utf8 COLLATE utf8_bin;
     """
     cursor.execute(sql)
     db.commit()
 
 dropalltable()
+createshopstable()
 creategoodstable()
+createpricetypestable()
 createpricestable()
 db.close()
