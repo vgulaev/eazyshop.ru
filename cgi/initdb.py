a68b913a42bb77f1b997e49cd2d17b18002e8c5a
Valentin <vgulaev@yandex.ru>
2013-10-27 16:56:48 +0600

    up

--- cgi/initdb.py
+++ cgi/initdb.py
@@ -1,4 +1,22 @@
 ﻿#!c:/Python27/python.exe
+
+import MySQLdb
 print ("Content-Type: text/html; charset=utf-8")
 print ("")
 print ("Hello word")
+
+db = MySQLdb.connect(host="localhost", user="root", passwd="mysql", db="eazyshop_db", charset='utf8')
+
+cursor = db.cursor()
+
+sql = """
+CREATE TABLE goods (
+caption CHAR(100)
+)
+"""
+
+cursor.execute(sql)
+
+db.commit()
+
+db.close()
