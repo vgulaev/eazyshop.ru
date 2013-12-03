0591c64ff03dfd7aa36902d72db6b587e0fd01cd
Valentin <vgulaev@yandex.ru>
2013-12-04 01:30:17 +0600

    Up

--- cgi/htmlgenerator.py
+++ cgi/htmlgenerator.py
@@ -1,4 +1,9 @@
 #!c:/Python27/python.exe
-
+def at(atname, value):
+    r = ""
+    if (value != ""):
+        r = atname + ' = "' + value + '"'
+    return r
+    
 def wh(text, tag, att = ""):
     return "<" + tag + " " + att + ">" + text + "</" + tag + ">"
