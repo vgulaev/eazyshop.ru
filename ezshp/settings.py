80e296a57952830b481b92c47e44234e7be43398
root <root@server.domain.com>
2013-12-07 19:36:05 +0600

    up

--- ezshp/settings.py
+++ ezshp/settings.py
@@ -15,9 +15,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'c9',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': os.environ['C9_USER'],
+        #'USER': os.environ['C9_USER'],
         'PASSWORD': '',
-        'HOST': os.environ['IP'],                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        #'HOST': os.environ['IP'],                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
