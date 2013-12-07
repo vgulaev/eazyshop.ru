82b4b579f6396646444ce4212bc027481ada73a4
Valentin <vgulaev@yandex.ru>
2013-12-07 14:40:17 -0500

    up

--- cgi/htmlgen.py
+++ cgi/htmlgen.py
@@ -3,33 +3,46 @@
 import htmlgenerator as hg
 
 class htmltag:
-    def __init__(self):
-        self.id = ""
-        self._class = ""
+    def __init__(self, id, _class):
+        self.id = id
+        self._class = _class
     def idtext(self):
 	    return hg.at("id", self.id) + hg.at("class", self._class)
 
 class a(htmltag):
-    def __init__(self):
-        htmltag.__init__(self)
+    def __init__(self, id = "", _class = ""):
+        htmltag.__init__(self, id, _class)
         self.href = ""
         self.caption = ""
     def text(self):
         return "<a " + self.idtext() + hg.at("href", self.href) + ">"+ self.caption + "</a>"
 
 class button(htmltag):
-    def __init__(self):
-        htmltag.__init__(self)
+    def __init__(self, id = "", _class = ""):
+        htmltag.__init__(self, id, _class)
         self.caption = ""
     def text(self):
 		return '<button' +  self.idtext() + '>' + self.caption + '</button>'
 
 class input(htmltag):
-    def __init__(self):
-        htmltag.__init__(self)
+    def __init__(self, id = "", _class = ""):
+        htmltag.__init__(self, id, _class)
 	def text(self):
 		return '<input' + self.idtext() + '></input>'
-
+		
+class registerblock(htmltag):
+    def __init__(self, id = "", _class = ""):
+        htmltag.__init__(self, id, _class)
+        self.items = []
+        t = input(id = "UserNameEmail")
+        self.items.append(t)
+    def text(self):
+        r = ""
+        for i in self.items:
+            r += i.text
+        r = hg.wh("", "<div>", self.idtext())
+        return r
+    
 class htmlgn:
     def __init__(self):
         self.items = []
--- ezshp/views.py
+++ ezshp/views.py
@@ -8,24 +8,21 @@ def index(request):
     
     a = hg.a()
     a.id = "sa"
-    a.caption = str(settings.STATICFILES_DIRS)
-    #a.caption = "Not bad"
+    #a.caption = str(settings.STATICFILES_DIRS)
+    a.caption = "Not bad"
     a.href = "/json/"
     t.items.append(a)
     
-    a = hg.a()
-    a.id = "sb"
+    a = hg.a(id = "sb")
     a.caption = "Show request"
     a.href = "/sr/"
     t.items.append(a)
 
-    b = hg.button()
-    b.id = "sb1"
+    b = hg.button(id = "sb1")
     b.caption = "Press me!"
     t.items.append(b)
     
-    b = hg.button()
-    b.id = "sb2"
+    b = hg.button(id = "sb2")
     b.caption = "Check me!"
     t.items.append(b)
 
