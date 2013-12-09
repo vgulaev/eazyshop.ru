77ce62c51860e8045dc613b9b3ec474a89accba4
vgulaev <vgulaev@yandex.ru>
2013-12-09 16:42:20 +0500

    Up

--- ezshp/htmlgenerator.py
+++
@@ -1,9 +0,0 @@
-#!c:/Python27/python.exe
-def at(atname, value):
-    r = ""
-    if (value != ""):
-        r = " " + atname + ' = "' + value + '"'
-    return r
-    
-def wh(text, tag, att = ""):
-    return "<" + tag + " " + att + ">" + text + "</" + tag + ">"
