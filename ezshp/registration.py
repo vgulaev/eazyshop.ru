89bb3810d580cdecbf2487c60f2e9573fdefc2e4
Valentin <vgulaev@yandex.ru>
2013-12-13 05:39:26 -0500

    up

--- ezshp/registration.py
+++ ezshp/registration.py
@@ -11,13 +11,19 @@ def index(request):
     t.append(n)
     t.append(hg.htmltext("<br>"))
     
-    t.append(hg.htmltext("""<div id = "registration-main-div">Пройдите три простых шага:
+    reg = hg.htmltext("""<div id = "registration-main-div">Пройдите три простых шага:
         <ol>
-        <li><div>Укажите свой электронный ящик<div><input id = "reg-email" placeholder = "e-mail"></input><button onclick = >Зарегистрироваться</button></li>
+        <li><div>Укажите свой электронный ящик<div><input id = "reg-email" placeholder = "e-mail"></input><button onclick = "sendregistermail()">Зарегистрироваться</button></li>
         <li>Подвердите свой ящик, перейдя по ссылке в письме</li>
         <li>Выберите версию 1С, которую Вы используете</li>
-        </ol></div"""))
+        </ol></div""")
 
+    t.append(reg)
+
+    t.jsmain = """function sendregistermail(){
+        alert("Helo!")
+    }
+    """
     t.cssmain = """#reg-email {
         width: 325px
     }"""
