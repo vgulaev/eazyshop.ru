4f25d5b2d05e606b3dce834869b7d9c49c85cd6c
vgulaev <vgulaev@yandex.ru>
2013-12-25 23:14:50 +0500

    Up

---
+++ static/js/index.js
@@ -0,0 +1,51 @@
+function authorize(){
+    var jqxhr = $.ajax({
+        "url":"/jsonws/ws/",
+        type: "POST",
+        "data": {
+            method    : "authorize",
+            login     : $("#login").val(),
+            pass      : $("#pass").val()
+        },
+        beforeSend: function () {
+            $("#button-signin").html("Идет авторизация");
+            $("#button-signin").attr("disabled", true);
+        }
+    } )
+    .done(function() {
+            //$("#button-signin").attr("disabled", false);
+            location.reload();
+        })
+    .fail(function() {
+        alert( "error" );
+    })
+    .always(function() {
+        $("#button-signin").html("Войти");
+        $("#button-signin").attr("disabled", false);
+    });
+}
+
+function logout(){
+    var jqxhr = $.ajax({
+        "url":"/jsonws/ws/",
+        type: "POST",
+        "data": {
+            method    : "logout",
+        },
+        beforeSend: function () {
+            $("#button-logout").html("Выходим...");
+            $("#button-logout").attr("disabled", true);
+        }
+    } )
+    .done(function() {
+            //$("#button-signin").attr("disabled", false);
+            location.reload();
+        })
+    .fail(function() {
+        alert( "error" );
+    })
+    .always(function() {
+        $("#button-logout").html("Войти");
+        $("#button-logout").attr("disabled", false);
+    });
+}
