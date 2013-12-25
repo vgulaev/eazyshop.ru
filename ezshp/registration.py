567c8e43f89971ddd083d5f09c2b49f791a9e34c
vgulaev <vgulaev@yandex.ru>
2013-12-25 20:27:46 +0500

    Up

--- ezshp/registration.py
+++ ezshp/registration.py
@@ -7,56 +7,10 @@ import json
 from Crypto.Cipher import AES
 
 def index(request):
-    # t = hg.htmlgn(request)
-    # t.setjsrootname(request.path)
-
-    # n = hg.navigationblock()
-    # t.append(n)
-    # t.append(hg.htmltext("<br>"))
-    
-    # reg = hg.htmltext("""<div id = "registration-main-div">Пройдите три простых шага:
-    #     <ol>
-    #     <li id = "point-one"><div>Укажите свой электронный ящик<div><input id = "reg-email" placeholder = "e-mail"></input><button id = "sender" onclick = "sendregistermail()">Зарегистрироваться</button></li>
-    #     <li>Подвердите свой ящик, перейдя по ссылке в письме</li>
-    #     <li>Выберите версию 1С, которую Вы используете</li>
-    #     </ol></div""")
-
-    # t.append(reg)
-
-    # t.jsmain = """function sendregistermail(){
-    # var jqxhr = $.ajax({
-    # "url":"/jsonws/ws/",
-    # type: "POST",
-    # "data": {
-    #     method:"sendacceptedmail",
-    #     email:$("#reg-email").val()},
-    # beforeSend: function () {
-    #         $("#sender").html("Идет обработка");
-    #         $("#sender").attr("disabled", true);
-    # }
-    # } )
-    #     .done(function() {
-    #         //alert( "success" );
-    #         alert( "На Ваш адресс " + $("#reg-email").val() + " выслано письмо с дальнейшими действиями.");
-    #         $("#reg-email").attr("disabled", true);
-    #         $("#point-one").css("text-decoration", "line-through");
-    #     })
-    #     .fail(function() {
-    #         alert( "error" );
-    #     })
-    #     .always(function() {
-    #         //alert( "На Ваш адресс " + $("#reg-email").val() + " выслано письмо с дальнейшими действиями.");
-    #         $("#sender").html("Зарегистрироваться");
-    #     });
-    # }
-    # """
-    # t.cssmain = """#reg-email {
-    #     width: 325px
-    # }"""
-    # httptext = t.gen()
     links = ["registration.css"]
     scripts = ["registration.js"]
-    context = {}
+    context = { "scripts": scripts,
+                "links": links}
     return render(request, 'registration.html', context)
 
 def accept(request, uid = ""):
