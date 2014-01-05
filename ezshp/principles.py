634e439c4632f0489e7641effa679c1ac34c08d1
vgulaev <vgulaev@yandex.ru>
2014-01-05 21:00:52 +0500

    Move to template

--- ezshp/principles.py
+++ ezshp/principles.py
@@ -1,41 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
+from django.shortcuts import render
+import ez
 import htmlgen as hg
 
 def index(request):
-    t = hg.htmlgn(request)
-    t.setjsrootname(request.path)
-
-    n = hg.navigationblock()
-    t.append(n)
-    t.append(hg.htmltext("<br>"))    
-    
-    t.append(hg.htmltext("""
-    <ul>
-    <li>На первом месте функциональность</li>
-    <li>Аскетичный дизайн или его отсутсивие (возможен пересмотр)</li>
-    <li>Реализуеться функционал и публикуеться, дизайна может и не быть</li>
-    <li>Абсолютная открытость проекта: ваши данные абсолютно открытна, практически любой может их получить и как то использовать</li>
-    </ul>
-    """))
-    httptext = t.gen()
-    return HttpResponse(httptext)
+    authorities = ez.checkauthorize(request)
+    context = {"authorities": authorities}
+    return render(request, 'principles.html', context)
     
 def who_we_are(request):
-    t = hg.htmlgn(request)
-    t.setjsrootname(request.path)
-
-    n = hg.navigationblock()
-    t.append(n)
-    t.append(hg.htmltext("<br>"))    
-    
-    t.append(hg.htmltext("""Мы амбициозный проект, у которого есть несколько целей
-    <ul>
-    <li>Дать возможность каждому попробывать on-line торговлю</li>
-    <li>Стать лучшей интернет витриной</li>
-    <li>Предоставить всем сервис личных кабинетов как b2b так и b2c</li>
-    <li>Организовать удобный сервис формирование любых заказов</li>
-    </ul>
-    """))
-    httptext = t.gen()
-    return HttpResponse(httptext)
+    authorities = ez.checkauthorize(request)
+    context = {"authorities": authorities}
+    return render(request, 'who-we-are.html', context)
