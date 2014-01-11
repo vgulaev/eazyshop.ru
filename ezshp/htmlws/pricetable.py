b1852d6dd7b833512f0ddd38f7666ef56911d543
vgulaev <vgulaev@yandex.ru>
2014-01-11 20:26:36 +0500

    Up

--- ezshp/htmlws/pricetable.py
+++ ezshp/htmlws/pricetable.py
@@ -3,21 +3,24 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import myadmin.initdb
 from django.shortcuts import render
+from collections import namedtuple
 
 @csrf_exempt
 def index(request):
 	db = myadmin.initdb.dbworker()
 	substr = request.POST.get("substr")
 	if ((substr is None) or (substr == "")):
-		sql = "select caption from (select caption FROM goods ORDER BY RAND() LIMIT 10) as main order by caption"
+		sql = "select caption, id from (select caption FROM goods ORDER BY RAND() LIMIT 10) as main order by caption"
 	else:
-		sql = u"select caption FROM goods where caption like '%{0}%' ORDER BY caption LIMIT 10".format(substr)
+		sql = u"select caption, id FROM goods where caption like '%{0}%' ORDER BY caption LIMIT 10".format(substr)
 	#print(sql)
 	db.cursor.execute(sql)
 	row =  db.cursor.fetchone()
 	goods = []
+	Good = namedtuple('Good', ['caption', 'id'])
+
 	while row is not None:
-		goods += [row[0]]
+		goods += [Good(row[0], row[1])]
 		row =  db.cursor.fetchone()
 	context = {"deguging" : False,
 	"goods" : goods}
