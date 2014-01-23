26ea81c4a067a9fbbdea97083bbcd62ae8ea28a9
vgulaev <vgulaev@yandex.ru>
2014-01-23 19:14:46 +0500

    Up

--- static/js/gchoice.js
+++ static/js/gchoice.js
@@ -1,12 +1,15 @@
+it = _.template($('#item-template').html());
+
 function updatechoicetable() {
 	db = openDatabase("goods", "0.1", "A list of to do items.", 2000);
-    db.transaction(function(tx) {
-        tx.executeSql("select * from gchoice", [], function (trx, result) {
-    for (var i=0; i < result.rows.length; i++) {
-      $('body').append(result.rows.item(i).id); // <-- getting Field1 value
-    }
-  }, null);
-        });
+	db.transaction(function(tx) {
+		tx.executeSql("select * from gchoice", [], function (trx, result) {
+			
+			for (var i=0; i < result.rows.length; i++) {
+      			$('#tbodychoice').append(it({"title": result.rows.item(i).id})); // <-- getting Field1 value
+  			}
+		}, null);
+	});
 }
 
 $(function () {
