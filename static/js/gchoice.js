b6ed1b3eb95954c2a628aa153ccdef2b48937286
vgulaev <vgulaev@yandex.ru>
2014-01-28 14:52:09 +0500

    Up

--- static/js/gchoice.js
+++ static/js/gchoice.js
@@ -59,20 +59,7 @@ function TableChoice($scope) {
 		});	*/
 	};
 }
-//it = _.template($('#item-template').html());
-
-/*function updatechoicetable() {
-	db = openDatabase("goods", "0.1", "A list of to do items.", 2000);
-	db.transaction(function(tx) {
-		tx.executeSql("select * from gchoice", [], function (trx, result) {
-			
-			for (var i=0; i < result.rows.length; i++) {
-      			//$('#tbodychoice').append(it({"title": result.rows.item(i).id})); // <-- getting Field1 value
-      		}
-      	}, null);
-	});
-}
 
 $(function () {
-	updatechoicetable();
-})*/
+	$("#output2").html(localStorage.length);
+})
