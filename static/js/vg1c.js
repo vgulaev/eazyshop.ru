9c7876805c1d16f783f53c86638402cf9eed9908
vgulaev <vgulaev@yandex.ru>
2014-02-14 15:47:32 +0500

    suds for 1C

--- static/js/vg1c.js
+++ static/js/vg1c.js
@@ -1,6 +1,22 @@
-/*function vg1c () {
-	DocumentList
+function suds1c () {
+  var soapquery = function () {
+    var xstr = '<?xml version="1.0" encoding="UTF-8"?>'+
+    '<SOAP-ENV:Envelope xmlns:ns0="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">' +
+    '<SOAP-ENV:Header/>'+
+    '<ns0:Body>'+
+    '</ns0:Body>'+
+    '</SOAP-ENV:Envelope>';
+    return $.parseXML(xstr);
+  }
+
+  var soapclient = function () {
+  };
+
+  this.client = function (paramobj) {
+    return soapquery();
+  };
 }
 
-$(
-	);*/
+cl = new suds1c;
+cl.client({"url" : "http://89.250.147.200:40080/USODev2014/ws/restservice.1cws",
+ "wsdl": "http://89.250.147.200:40080/USODev2014/ws/restservice.1cws?wsdl"});
