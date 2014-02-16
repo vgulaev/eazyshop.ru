e5ea0a5295c4f17d32817c1ee7fb5fecab93acbb
vgulaev <vgulaev@yandex.ru>
2014-02-16 17:48:34 +0500

    Up

--- static/js/vg1c.js
+++ static/js/vg1c.js
@@ -14,7 +14,6 @@ function suds1c () {
 
   this.client = function (paramobj) {
     res = new soapclient;
-
     $.ajax({
       "url" : paramobj["wsdl"],
       "type": "GET",
@@ -30,7 +29,6 @@ function suds1c () {
         res[methodname] = function (methodname) {
           return function (params) {
             var soapq = soapquery();
-            //soapq.documentElement.setAttribute("xmlns:ns1", namespace);
             $(soapq.documentElement).attr("xmlns:ns1", namespace);
             var body = soapq.documentElement.getElementsByTagNameNS("http://schemas.xmlsoap.org/soap/envelope/", "Body")[0];
             var el = soapq.createElement("ns1:" + methodname);
@@ -49,9 +47,8 @@ function suds1c () {
               "data": xstr
             })
             .done(function (data) {
-              //var res = data.evaluate("/*[local-name()='Envelope']/*[local-name()='Body']/*/*", data, null, 9, null).singleNodeValue;
               var res = $(data).find("return").text();
-              params["done"]();
+              params["done"](res);
             });
           }
         }(methodname);
@@ -59,22 +56,76 @@ function suds1c () {
     })
 .fail(function () {
 });
-
-
 return res;
 };
 }
 
-cl = new suds1c;
-ws = cl.client({"url" : "http://127.0.0.1/USODev2014/ws/restservice.1cws",
-  "wsdl": "http://127.0.0.1/USODev2014/ws/restservice.1cws?wsdl"});
-$("#output").html("Heloo!!!");
+/*function vg1c() {
 
-/*ws.helloword({"done" : function (data) {
-  alert(data)
+}*/
+
+function loadvg1cobject () {
+    vg1c = {};
+    $("[vg1csource]").each( function ( i ) {
+        var strobj = $(this).attr("vg1csource");
+        var defarray = strobj.split(".");
+        if (defarray[0] == "ДокументСписок") {
+          var objid = this.id;
+          vg1c[objid] = {};
+          vg1c[objid].view = this;
+          vg1c[objid].tablename = "Документ." + defarray[1];
+          var colums = "";
+          $(this).find("[vg1cfield]").each( function () {
+            colums = colums + $(this).attr("vg1cfield") + ",";
+          });
+          if (colums != "") {colums = colums.slice(colums, -1)};
+          vg1c[objid].colums = colums;
+
+          vg1c[objid].update = function (params) {
+            var first = params["first"];
+            var count = params["count"];
+            if (first == undefined) {
+                first = "0";
+            };
+            if (count == undefined) {
+                count = "10";
+            };
+            ws.gettable({
+              "data" : {
+                "tablename" : this.tablename,
+                "colums" : this.colums,
+                "firstrecord" : first,
+                "count" : count
+              },
+              "done" : function (data) {
+                var xmltable = $.parseXML(data);
+                var tbody = $(vg1c.tab.view).find("tbody");
+                tbody.empty();
+                var tdnames = colums.split(",");
+                $(xmltable).find("row").each(function () {
+                  var tr = document.createElement("tr");
+                  for (var i =  0; i < tdnames.length; i++) {
+                    var td = document.createElement("td");
+                    $(td).html($(this).attr(tdnames[i]));
+                    tr.appendChild(td);
+                  };
+                  tbody.append(tr);
+                });
+              }
+            });
+          }
+        }
+        var k = 1;
+    });
 }
-});*/
 
+$(function () {
+    cl = new suds1c;
+    ws = cl.client({"url" : "http://127.0.0.1/USODev2014/ws/restservice.1cws", "wsdl": "http://127.0.0.1/USODev2014/ws/restservice.1cws?wsdl"});
+
+    loadvg1cobject();
+    
+    vg1c.tab.update({});
 ws.getobj({
   "data" : {
     "uid" : "3081c8b7-9498-11e3-88b2-94de80b807e8",
@@ -82,7 +133,18 @@ ws.getobj({
     "variant" : "own",
   },
   "done" : function (data) {
-  alert(data)
-}
+    //alert(data)
+  }
+});
 });
 
+/*ws.getobj({
+  "data" : {
+    "uid" : "3081c8b7-9498-11e3-88b2-94de80b807e8",
+    "type" : "ВнутреннийЗаказ",
+    "variant" : "own",
+  },
+  "done" : function (data) {
+    alert(data)
+  }
+});*/
