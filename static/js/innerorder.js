f2653180d59a5612d71c362710a887818d2cdd8d
vgulaev <vgulaev@yandex.ru>
2014-02-18 19:18:23 +0500

    Up

---
+++ static/js/innerorder.js
@@ -0,0 +1,96 @@
+$(function () {
+	loadvg1cobject({"url" : "/ws/restservice.1cws", "wsdl": "/ws/restservice.wsdl"});
+	vg1c.innerorder.update({"uid" : $("[property='orderuid']").attr("content")});
+});
+
+function InnerOrders ($scope, dateFilter) {
+	$scope.order = {};
+	$scope.orderuid = $("[property='orderuid']").attr("content");
+
+	$scope.$watch('order.Date', function (date)
+    {
+        $scope.order.dateString = dateFilter(date, 'yyyy-MM-dd');
+    });
+
+    $scope.$watch('order.dateString', function (dateString)
+    {
+        $scope.order.Date = new Date(dateString);
+    });
+	//$scope.dateString dateString
+	$scope.update = function update() {
+		var sql = [];
+		var jqxhr = $.ajax({
+			"url":"/jsonws/ws/",
+			type: "POST",
+			"data": {
+				"method"        : "getorder",
+				"uid"			: $scope.orderuid
+			},
+			beforeSend: function () {
+				$("#ajaxing").css("left", (window.screen.availWidth - $("#ajaxing").width())/2 + "px");
+				$("#ajaxing").css("top", ($("#maincontainer").height()- $("#ajaxing").height()) / 2 + "px");
+				$("#ajaxing").show();
+			},
+		})
+		.done(function(data) {
+			/*$scope.lines = data.rows;
+			angular.forEach($scope.lines, function(value, key){
+				value["дата"] = dateFilter((new Date(value["дата"])), "yyyy-MM-dd");
+			});*/
+			$scope.order = data;
+			$scope.Date = new Date($scope.Date);
+			$scope.$apply();
+		})
+		.fail(function() {
+		})
+		.always(function() {
+			$("#ajaxing").hide();
+		});
+	}
+
+	/*$scope.closeorder = function closeorder() {
+		$("#page-order").hide();
+		$("#page-order-list").show();
+	}
+
+	$scope.openinnerorder = function openinnerorder (id1c) {
+		var s = ["select * from innerorder where id1C = '{id1C}'".replace("{id1C}", id1c), "select rownumber, caption, quantity, goods.unit from innerorder_goods join (goods) on (innerorder_goods.good = goods.id) where id1C = '{id1C}'".replace("{id1C}", id1c)];
+		var jqxhr = $.ajax({
+			"url":"/jsonws/db/",
+			type: "POST",
+			"data": {
+				"method"        : "queries",
+				"qtext"         : JSON.stringify(s)
+			},
+			beforeSend: function () {
+				$("#ajaxing").css("left", (window.screen.availWidth - $("#ajaxing").width())/2 + "px");
+				$("#ajaxing").css("top", ($("#maincontainer").height()- $("#ajaxing").height()) / 2 + "px");
+				$("#ajaxing").show();
+				$("#page-order-list").hide();
+				$("#page-order").show();
+			},
+		})
+		.done(function(data) {
+			if (data.results.length > 0) {
+				var headdata = data.results[0];
+				if (headdata.rows.length > 0){
+					var curent = headdata.rows[0];
+					$scope.order.date = new Date(curent[2]);
+				}
+				var tabledata = data.results[1];
+				if (tabledata.rows.length > 0) {
+					$scope.order.lines = tabledata.rows;
+				}
+				$scope.$apply();
+			}
+		})
+		.fail(function() {
+		})
+		.always(function() {
+			$("#ajaxing").hide();
+		});
+
+	}*/
+
+	$scope.update();
+}
