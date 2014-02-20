a91011446a83ebb57a5ea6aa045b6897e9a7a12c
vgulaev <vgulaev@yandex.ru>
2014-02-20 10:32:48 +0500

    Up

--- ezshp/myadmin/statistics.py
+++ ezshp/myadmin/statistics.py
@@ -6,14 +6,24 @@ def index(request):
 	path = __file__.split("\\")
 	rootdir = "\\".join(path[0:-3])
 	httptext = ""
-	stat = {}
+	#stat = {}
+	stat = {".css" : {}, ".js" : {}, ".django" : {}, ".py" : {}, ".html" : {}}
+	for e in stat:
+		stat[e] = {"items" : 0, "lines" : 0}
+	totallines = 0
 	for root, subFolders, files in os.walk(rootdir):
 		for fl in files:
 			fileName, fileExtension = os.path.splitext(fl)
+			f = open(root + "\\" + fl)
 			if ((fileExtension in stat) == True):
-				stat[fileExtension] = stat[fileExtension] + 1
-			else:
-				stat[fileExtension] = 0
+				stat[fileExtension]["items"] = stat[fileExtension]["items"] + 1
+				lines = len(f.readlines())
+				stat[fileExtension]["lines"] = stat[fileExtension]["lines"] + lines
+				totallines = totallines + lines
+			#else:
+			#	stat[fileExtension] = {"items" : 1, "lines" : len(f.readlines())}
+			f.close()			
 	for e in stat:
-		httptext = httptext + "<br>" + e + " " + str(stat[e])
+		httptext = httptext + "<br>" + e + " " + str(stat[e]["items"]) + " lines " + str(stat[e]["lines"])
+	httptext = httptext + "<br>" + str(totallines * 12) + " $"
 	return HttpResponse(httptext)
