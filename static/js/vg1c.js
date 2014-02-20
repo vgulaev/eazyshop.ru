b47d9663d380bef7286678ac9ea55866a05bbc01
vgulaev <vgulaev@yandex.ru>
2014-02-20 17:28:02 +0500

    Up

--- static/js/vg1c.js
+++ static/js/vg1c.js
@@ -65,6 +65,31 @@ function loadvg1cobject (params) {
       };
     }
     else if (defarray[0] == "ДокументОбъект") {
+      vg1c[objid]["push"] = {length : 0, items: {}};
+      vg1c[objid].pushtoserver = function () {
+        var fields = [];
+        var values = [];
+        for (i in vg1c[objid]["push"].items){
+          fields.push(i);
+          values.push(vg1c[objid]["push"].items[i]);
+        }
+        vg1c.ws.update({
+      "data" : {
+        "type" : "",
+        "uid" : $("meta[property='orderuid']").attr("content"),
+        "version" : "",
+        "field" : fields.join(","),
+        "value" : values.join(",")
+      },
+      "done" : function (data) {
+        vg1c.innerorder.update({"uid" : $("[property='orderuid']").attr("content")});       
+      }
+    });
+        vg1c[objid]["push"] = {length : 0, items: {}};
+        var bt = $( "[vg1ccommand = 'write']" );
+        $( bt ).hide();
+      };
+      $( "[vg1ccommand = 'write']" ).click(vg1c[objid].pushtoserver);
       vg1c[objid].update = function (params) {
         vg1c.ws.getobj({
           "data" : {
@@ -75,6 +100,7 @@ function loadvg1cobject (params) {
           "done" : function (data) {
             var xmltable = $.parseXML(data);
             $(vg1c[objid].view).attr("uid", params["uid"]);
+            $(vg1c[objid].view).attr("version", $(xmltable).find("DataVersion").text());
             $(vg1c[objid].view).find("input[vg1cfield]").each(function ( i ) {
               var node = $(xmltable).find($(this).attr("vg1cfield"));
               $(this).val(node.text());
@@ -89,18 +115,39 @@ function loadvg1cobject (params) {
             $(vg1c[objid].view).find("table[vg1ctable]").each(function ( i ) {
               var columns = $(this).find("[vg1ccolumn]");
               var tbody = $(this).find("tbody");
+              var index = 0;
+              var tablename = $(this).attr("vg1ctable");
               tbody.empty();
-              $(xmltable).find($(this).attr("vg1ctable") + ">Row").each(function () {
+              $(xmltable).find(tablename + ">Row").each(function () {
                 var tr = document.createElement("tr");
                 for (var i = 0; i < columns.length; i++) {
                   var td = document.createElement("td");
                   var fieldname = $(columns[i]).attr("vg1ccolumn");
-                  $(td).html($(this).find(fieldname).text());
+                  var represent = $(this).find(fieldname).text();
+                  var tdinner = represent;
+                  if ($(columns[i]).attr("vg1ceditable") == "true") {
+                    var tid = tablename + "[" + index + "]." + fieldname;
+                    tdinner = "<input id = '" + tid + "' value = '" + represent + "' class = 'choise-input' vg1ceditable = 'true'>";
+                  }
+                  $(td).html(tdinner);
                   tr.appendChild(td);
                 };
                 tbody.append(tr);
+                index = index + 1;
               });
             });
+            var onchangefunction = function () {
+              if (vg1c[objid].push.items[this.id] == undefined) {
+                vg1c[objid]["push"].length = vg1c[objid]["push"].length + 1;
+              }
+              vg1c[objid].push.items[this.id] = $(this).val();
+              var bt = $( "[vg1ccommand = 'write']" );
+              bt.show();
+              $( bt ).find("span").html(vg1c[objid]["push"].length);
+            };
+            $("input[vg1ceditable='true']").bind("change paste keyup", onchangefunction);
+            $("textarea[vg1ceditable='true']").bind("change paste keyup", onchangefunction);
+            var k = 1;
           }
         });
 }
@@ -119,29 +166,4 @@ $(function () {
     //vg1c.innerorder.update({"uid" : "3081c8b7-9498-11e3-88b2-94de80b807e8"});
     //vg1c.tab.update({});
     //vg1c.innerorder.hide();
-    
-    /*ws.getobj({
-  "data" : {
-    "uid" : "3081c8b7-9498-11e3-88b2-94de80b807e8",
-    "type" : "ВнутреннийЗаказ",
-    "variant" : "own",
-  },
-  "done" : function (data) {
-    //alert(data)
-  }
-});*/
-    //vg1c.tab.update({});
-    //vg1c.tab.hide();
-
-/*ws.getobj({
-  "data" : {
-    "uid" : "3081c8b7-9498-11e3-88b2-94de80b807e8",
-    "type" : "ВнутреннийЗаказ",
-    "variant" : "own",
-  },
-  "done" : function (data) {
-    alert(data)
-  }
-});*/
 });
-
